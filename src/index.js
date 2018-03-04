3af8bba135041990156cccfccd3bbe4c6d9988f0
thehouseplant <sean.collins@outlook.com>
2018-03-04 13:16:14 -0800

    Create-React-App base with a few tweaks

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'typeface-roboto';
+import './index.css';
+
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, document.getElementById('root')
+);
+
+registerServiceWorker();
