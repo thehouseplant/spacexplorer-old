d3a1f06563f84d95fe55620da4869b4e89dbc6c4
thehouseplant <sean.collins@outlook.com>
2018-03-05 20:47:19 -0800

    Added more fields and cleaned up views

--- src/components/CompanyInfo.js
+++ src/components/CompanyInfo.js
@@ -8,7 +8,8 @@ class CompanyInfo extends Component {
     super(props);
 
     this.state = {
-      info: {}
+      info: {},
+      headquarters: {}
     };
   }
 
@@ -21,7 +22,8 @@ class CompanyInfo extends Component {
       .get(`https://api.spacexdata.com/v2/info`)
       .then(res => this.setState(
         {
-          info: res.data
+          info: res.data,
+          headquarters: res.data.headquarters
         }
       ))
       .catch(err => console.log(err));
@@ -35,22 +37,43 @@ class CompanyInfo extends Component {
             <Typography variant="headline" component="h3">
               {this.state.info.name}
             </Typography>
+            <p>
+              {this.state.info.summary}
+            </p>
 
-            <Typography component="p">
+            <p>
               <strong>Founder</strong>: {this.state.info.founder}
-            </Typography>
-            <Typography component="p">
+            </p>
+            <p>
               <strong>Founded</strong>: {this.state.info.founded}
-            </Typography>
-            <Typography component="p">
+            </p>
+          
+            <strong>Headquarters</strong>:
+            <ul>
+              <li>{this.state.headquarters.address}</li>
+              <li>{this.state.headquarters.city}, {this.state.headquarters.state}</li>
+            </ul>
+            <p>
               <strong>Employees</strong>: {this.state.info.employees}
-            </Typography>
-            <Typography component="p">
+            </p>
+            <p>
               <strong>Vehicles</strong>: {this.state.info.vehicles}
-            </Typography>
-            <Typography component="p">
+            </p>
+            <p>
               <strong>Launch Sites</strong>: {this.state.info.launch_sites}
-            </Typography>
+            </p>
+            <p>
+              <strong>Test Sites</strong>: {this.state.info.test_sites}
+            </p>
+            <strong>Key Staff</strong>:
+            <ul>
+              <li>CEO: {this.state.info.ceo}</li>
+              <li>CTO: {this.state.info.cto}</li>
+              <li>COO: {this.state.info.coo}</li>
+            </ul>
+            <p>
+              <strong>Valuation</strong>: ~${this.state.info.valuation}
+            </p>
           </CardContent>
         </Card>
       </div>
